33e4d464bbf519b2d077b2e096f65caad397d3d7
Edgars Putka <v-edputk@microsoft.com>
2018-05-14 00:41:41 +0300

    HW5: Code optimisation using page objects. TODO: minor tweaks with validation

--- features/pages/home_page.rb
+++ features/pages/home_page.rb
@@ -2,9 +2,23 @@ class HomePage < BasePage
     attr_accessor :button_start
   def initialize
     @button_start = Element.new(:css, '#start_button')
-    @form_sign_up = Element.new(:css, '#signup')
     @button_close_try_now = Element.new(:css, '#signup .closecross')
     @input_name_contact_us = Element.new(:css, '#name')
+  #Sign Up
+    @button_sign_up_landing = Element.new(:css, '#signup-b')
+    @form_sign_up = Element.new(:css, '#signup')
+    @input_sign_up_email = Element.new(:css, '#signup input[name="login"]')
+    @input_sign_up_password = Element.new(:css, '#signup input[name="password1"]')
+    @input_sign_up_retry_password = Element.new(:css, '#signup input[name="password2"]')
+    @input_sign_up_project_name = Element.new(:css, '#signup input[name="project_name"]')
+    @button_sign_up_close = Element.new(:css, 'form[action="/user"] .closecross')
+  #Log In
+    @button_log_in_landing = Element.new(:css,'#login-b')
+    @form_log_in = Element.new(:css, '#login')
+    @input_log_in_email = Element.new(:css, '#login input[name="login"]')
+    @input_log_in_password = Element.new(:css, '#login input[name="password"]')
+    @button_log_in = Element.new(:css, '.button-block')
+    @error_invalid_credentials = Element.new(:css, 'form[action="/login"] div.alert.alert-danger span.errorText')
   end
 
   def isVisible
@@ -16,21 +30,55 @@ class HomePage < BasePage
     visit('/legacy')
   end
 
-  def openTryNow
-    @button_start.click
+  def openSignUp
+    @button_sign_up_landing.click
     @form_sign_up.isVisible
-    @button_close_try_now.isVisible
+    @button_sign_up_close.isVisible
   end
 
-  def closeTryNow
-    @button_close_try_now.click
+  def enterSignUpDetails(email, password1, password2, projectName)
+    @input_sign_up_email.send_keys(email)
+    @input_sign_up_password.send_keys(password1)
+    @input_sign_up_retry_password.send_keys(password2)
+    @input_sign_up_project_name.send_keys(projectName)
   end
 
-  def enterName(name)
-    @input_name_contact_us.send_keys(name)
+  def closeSignUp
+    @button_sign_up_close.click
   end
 
-  def clearName
-    @input_name_contact_us.clear
+  def openLogIn
+    @button_log_in_landing.click
+    @form_log_in.isVisible
   end
+  
+  def enterLogInDetails(email, password)
+    @input_log_in_email.send_keys(email)
+    @input_log_in_password.send_keys(password)
+  end
+
+  def selectLoginButton
+    @button_log_in.click
+  end
+
+  def validateInvalidCredentialsError
+    @error_invalid_credentials.isVisible
+  end
+  # def openTryNow
+  #   @button_start.click
+  #   @form_sign_up.isVisible
+  #   @button_close_try_now.isVisible
+  # end
+
+  # def closeTryNow
+  #   @button_close_try_now.click
+  # end
+
+  # def enterName(name)
+  #   @input_name_contact_us.send_keys(name)
+  # end
+
+  # def clearName
+  #   @input_name_contact_us.clear
+  # end
 end
--- features/step_definitions/signup_steps.rb
+++ features/step_definitions/signup_steps.rb
@@ -1,46 +1,82 @@
-When(/^I am on Appimation home page/) do
-  # visit('/')
-  # unless find(:css, '#logo').visible?
-  #   raise "Logo not visible"
-  # end
+When("I am on Appimation home page") do
   @pages.page_home.load
   @pages.page_home.isVisible
 end
 
 
-
-When(/^I open Try now/) do
-  # find(:css, '#start_button').click
-  # unless find(:css, '#signup').visible?
-  #   raise "element not visible"
-  # end
-  @pages.page_home.openTryNow
+##Scenario 1: Enter Sign Up details and close view
+When("I select Sign Up button") do
+  @pages.page_home.openSignUp
+end
+When("I enter {string}, {string}, {string}, {string} in Sign Up fields") do |email, password1, password2, projectName|
+  @pages.page_home.enterSignUpDetails(email, password1, password2, projectName)
+end
+Then("I close Sign Up screen") do
+  @pages.page_home.closeSignUp
 end
 
 
-Then("I close Try now") do
-  # find(:css, '#signup .closecross').click
-  @pages.page_home.closeTryNow
+## Scenario 2: Log in with invalid credentials
+When("I select Log In button") do
+  @pages.page_home.openLogIn
+end
+When("I enter {string}, {string} in Log In fields") do |email, password|
+  @pages.page_home.enterLogInDetails(email, password)
+end
+Then("I select Login") do
+  @pages.page_home.selectLoginButton
+end
+Then("I validate if error is visible") do
+  @pages.page_home.validateInvalidCredentialsError
 end
 
 
-When("I enter {string} and {string} in name in contact us") do |name, name2|
-  # unless find(:css, '#name').visible?
-  #   raise "name not visible"
-  # end
-  # find(:css, '#name').send_keys(name)
-  # sleep(2)
-  # find(:css, '#name').native.clear
-  #
-  # find(:css, '#name').send_keys(name2)
-  # sleep(2)
+# ###########
 
-  @pages.page_home.enterName(name)
-  @pages.page_home.clearName
-  sleep(2)
-  @pages.page_home.enterName(name2)
-  sleep(2)
-end
+
+# When(/^I am on Appimation home page/) do
+#   # visit('/')
+#   # unless find(:css, '#logo').visible?
+#   #   raise "Logo not visible"
+#   # end
+#   @pages.page_home.load
+#   @pages.page_home.isVisible
+# end
+
+
+
+# When(/^I open Try now/) do
+#   # find(:css, '#start_button').click
+#   # unless find(:css, '#signup').visible?
+#   #   raise "element not visible"
+#   # end
+#   @pages.page_home.openTryNow
+# end
+
+
+# Then("I close Try now") do
+#   # find(:css, '#signup .closecross').click
+#   @pages.page_home.closeTryNow
+# end
+
+
+# When("I enter {string} and {string} in name in contact us") do |name, name2|
+#   # unless find(:css, '#name').visible?
+#   #   raise "name not visible"
+#   # end
+#   # find(:css, '#name').send_keys(name)
+#   # sleep(2)
+#   # find(:css, '#name').native.clear
+#   #
+#   # find(:css, '#name').send_keys(name2)
+#   # sleep(2)
+
+#   @pages.page_home.enterName(name)
+#   @pages.page_home.clearName
+#   sleep(2)
+#   @pages.page_home.enterName(name2)
+#   sleep(2)
+# end
 
 
 
